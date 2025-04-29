7b4fd78b87935dbaac97ff814ecea350f5da575f
Novashadow274 <Novashadow274@gmail.com>
2025-04-30 00:06:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 from threading import Thread
+from waitress import serve
 from server import app
 from scheduler import run
 
 def run_flask():
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+    serve(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
 
 if __name__ == "__main__":
-    Thread(target=run_flask, daemon=True).start()
+    flask_thread = Thread(target=run_flask, daemon=True)
+    flask_thread.start()
     run()
