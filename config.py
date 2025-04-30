c9d9979e660e5b82785ff29cb41cfeace767b5d6
Novashadow274 <Novashadow274@gmail.com>
2025-04-30 16:14:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+import os  
+from dotenv import load_dotenv  
+  
+load_dotenv()  
+  
+BOT_TOKEN = os.getenv("BOT_TOKEN")  
 CHANNEL_ID = os.getenv("CHANNEL_ID")
-NITTER_INSTANCE = os.getenv("NITTER_INSTANCE", "https://nitter.net")
